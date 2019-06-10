03be1579ab50e430e426f9229a2df4262cc338da
박상훈 <tkdgnszhekr@gmail.com>
2019-06-10 18:56:21 +0900

    첫 업로드입니다

---
+++ Assets/Script/SungminScripts/UIFuncScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIFuncScript : MonoBehaviour
+{
+    private bool pauseState = false;
+
+    [SerializeField]
+    private GameObject homeButton;
+
+    [SerializeField]
+    private Animation[] uiAnime;
+
+    void OnHint()
+    {
+
+    }
+
+    void OnReStart()
+    {
+        SceneManager.LoadScene(3);
+    }
+    
+    void OnOffPause()
+    {
+        pauseState = !pauseState;
+
+        if (pauseState)
+        {
+            homeButton.SetActive(false);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            homeButton.SetActive(true);
+            Time.timeScale = 1;
+        }
+    }
+
+    void BackScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    void GameClearAnime()
+    {
+        for (int i = 0; i < uiAnime.Length; i++)
+            uiAnime[i].Play();
+    }
+}
